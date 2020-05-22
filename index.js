af552eb18e29b7d6534b41e37c2fabb8b414170c
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2020-05-21 23:09:13 -0400

    simple output to cli when run: node index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+
+clear();
+
+console.log(
+    chalk.yellow(
+        figlet.textSync('Ginit', { horizontalLayout: 'full' })
+    )
+);
