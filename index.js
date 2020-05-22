3d1f8cb17de3bfaba61d071731d9574bae7ce9cc
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2020-05-21 23:20:58 -0400

    check if github repo already exists

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+const log = console.log
 
 clear();
 
-console.log(
+log(
     chalk.yellow(
         figlet.textSync('Ginit', { horizontalLayout: 'full' })
     )
 );
+
+if (files.directoryExists('.git')) {
+    log(chalk.bold.blue("Github repo already exists"));
+    process.exit();
+}
